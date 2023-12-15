dd1b94ba85542180b5f55bbfbee3f3f47d5f5f51
unknown <ndobany@gmail.com>
2023-12-15 10:02:42 +0100

    commit 1 main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+magassag = input("magassag: ")
+
+"""
+milyen objektumok?:
+git add
+    blob
+
+git commit
+    tree
+    commit
+
+file/
+    main.py
+    1.py
+    2.py
+    3.py
+
+git add main.py
+git add 1.py ect...
+
+git status -u
+git add --all
+git add .
+
+user check:
+    git config --global user.name
+user delete
+    git config --global --unset user.name
+user add
+    git config --global user.email ""
+    
+Git upload:
+    git remote add origin https://github.com/notdefinitelynot/git_gyak
+
+Windows hitelesítő adatok
+Windows rendszerbeli hitelesítő adatok
+
+Upload:
+git push
+"""
